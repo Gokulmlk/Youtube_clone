0b21d7be390b4ffd0d767d515a2655af8ac39f99
Gokul Malik <gokulmlk21@gmail.com>
2026-03-03 15:21:55 +0530

    app created in backend

---
+++ backend/script.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req,res)=>{
+    res.send("hello from servier")
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is runningin ${PORT}` )
+})
