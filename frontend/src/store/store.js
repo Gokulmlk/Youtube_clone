23057bb17250d2b17ec5a4c3fdb4237d347461b5
Gokul Malik <gokulmlk21@gmail.com>
2026-03-05 01:10:47 +0530

    store created to handle the slice fro redux

---
+++ frontend/src/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slices/authSlice'
+import videoReducer from './slices/videoSlice'
+import channelReducer from './slices/channelSlice'
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    video: videoReducer,
+    channel: channelReducer,
+  },
+})
